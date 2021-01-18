de9fe1ae05fcb3b85d035b714c9a0b5f175715e9
Vlad Vavilin <vlad.vavilin73@gmail.com>
2021-01-18 23:16:00 +0500

    Again a bit of code refactoring. Composing interfaces ( It seeems, that I even overdid it :) )

--- internal/app/service/services.go
+++ internal/app/service/services.go
@@ -8,30 +8,32 @@ import (
 
 type (
 	UserService interface {
-		//Crud methods
-		FindUserByID(ctx context.Context, userID string, fields *store.UserFields) (*model.User, error)
-		//FindUserByName(ctx context.Context, username string, fields *store.UserFields) (*model.User, error)
-		//FindUserByEmail(ctx context.Context, email string, fields *store.UserFields) (*model.User, error)
-		//
+		UserCrud
 		UserSessionsFinder
-		//
-		UpdateUserInfo(ctx context.Context, userID string, userinfo map[string]string) error
-		//
-		Registration(ctx context.Context, username, password string) error
-		ConfirmEmail(ctx context.Context, user *model.User, token string) error
-		//
-		//
 		UserAuthenticator
 	}
-	UserAuthenticator interface {
-		Authenticate(ctx context.Context, login, password, clientID string) (*model.Identity, error)
-		UpdateToken(ctx context.Context, userID, clientID, refToken string) (*model.Identity, error)
-		SignOut(ctx context.Context, userID, sessionID string) error
+
+	UserFinder interface {
+		FindUserByID(ctx context.Context, userID string, fields *store.UserFields) (*model.User, error)
+		FindUserByLogin(ctx context.Context, login string, fields *store.UserFields) (*model.User, error)
+		FindUserByName(ctx context.Context, username string, fields *store.UserFields) (*model.User, error)
+		FindUserByEmail(ctx context.Context, email string, fields *store.UserFields) (*model.User, error)
 	}
 	UserSessionsFinder interface {
 		FindUserSessions(ctx context.Context, userID string) (*[]model.UserSession, error)
 	}
-	//
+	UserCrud interface {
+		UserFinder
+		UpdateUserInfo(ctx context.Context, userID string, userinfo map[string]string) error
+		Registration(ctx context.Context, user *model.User, password string) error
+		ConfirmEmail(ctx context.Context, user *model.User, token string) error
+	}
+	UserAuthenticator interface {
+		Authenticate(ctx context.Context, login, password, clientID string) (*model.Identity, error)
+		UpdateRefToken(ctx context.Context, userID, clientID, refToken string) (*model.Identity, error)
+		SignOut(ctx context.Context, userID, sessionID string) error
+	}
+
 	ClientService interface {
 		FindClientByID(ctx context.Context, clientID string) (*model.Client, error)
 	}
